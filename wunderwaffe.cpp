f173e1e18440078f1cf2035e8c87f7f782fbd318
babenek <babenek@users.noreply.github.com>
2016-03-05 10:13:38 +0200

    added comments and WUNDREWAFFE macro

--- wunderwaffe.cpp
+++ wunderwaffe.cpp
@@ -3,6 +3,12 @@
 #include <cstdint>
 #include <vector>
 
+//! Issue: gcov discover compilator generated branches in cpp files.
+//! the branches may be not executed - so branch coverage percent is low.
+//! if you have to increase the coverage percent with gcov - try WUNDERWAFFE 
+//! (similar as VoksWagen test:)
+
+//! macros definitions to create dummy branches
 #define bit0x01 if(n&0x01) {printf("\n true:%d",__COUNTER__ );} else {printf("\nfalse:%d",__COUNTER__ );}
 #define bit0x02 if(n&0x02) {bit0x01;} else {bit0x01;}
 #define bit0x04 if(n&0x04) {bit0x02;} else {bit0x02;}
@@ -12,11 +18,14 @@
 #define bit0x40 if(n&0x40) {bit0x20;} else {bit0x20;}
 #define bit0x80 if(n&0x80) {bit0x40;} else {bit0x40;}
 
+//! Creates dummy branches. Effective input: 1 - 255
+#define WUNDERWAFFE(NUMBER_OF_BRANCHES) {for(unsigned int n=0xFF & NUMBER_OF_BRANCHES; n!=0; --n){bit0x80;};}
+
 int main()
 {
-	for(int n =0;n<256;++n){
-		bit0x80;
-	}
+	WUNDERWAFFE(255);
+	//!< just do it to increase branch coverage with gcov :)
+	
 	printf("%d",__COUNTER__ );
 	printf("%d",__COUNTER__ );
 	return 0;
