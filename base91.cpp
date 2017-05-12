46ed6a8033565304dbbe0244d573d59deec005a6
Roman Babenko <babenek@gmail.com>
2017-05-13 00:42:19 +0300

    commit and sleep

--- base91.cpp
+++ base91.cpp
@@ -5,7 +5,7 @@
 #include <cstring>
 #include <stdexcept>
 #include <cassert>
-
+#include <iostream>
 
 //------------------------------------------------------------------------------
 namespace base91
@@ -15,6 +15,11 @@ static const unsigned char_bits=8;
 static const unsigned digit_bits=13;
 static const unsigned digit_size=0x2000;
 static const unsigned digit_mask=0x1FFF;
+
+char hi[digit_size]={'~',};
+char lo[digit_size]={'~',};
+
+unsigned short hilo[base][base]={0,};
 
 void encode(const std::vector<unsigned char> & in,std::string & out)
 {
@@ -38,20 +43,59 @@ void encode(const std::vector<unsigned char> & in,std::string & out)
 		}
 	}
 
-	while(0 < bitacc)
+	if(0 < bitacc)
 	{
 		const unsigned cod = digit_mask & acc;
 		out.push_back(lo[cod]);
-		out.push_back(hi[cod]);
-		acc>>=digit_bits;
-		bitacc-=digit_bits;
+		if(bitacc>=7)
+			out.push_back(hi[cod]);
 	}
-	
 	
 	return;
 }
 void decode(const std::string & in,std::vector<unsigned char> & out)
 {
+	out.clear();
+	out.reserve(char_bits*in.size()/digit_bits);
+
+	unsigned acc=0;
+	int bitacc=0;
+	short lower=-1;
+	
+	for (auto & n:in)
+	{
+		if(-1==lower)
+		{
+			lower=n-'!';
+			continue;
+		}
+		acc<<=digit_bits;
+		acc|=hilo[n-'!'][lower];
+		bitacc+=digit_bits;
+		lower=-1;
+		
+		while(char_bits<=bitacc)
+		{
+			out.push_back(0xFF & acc);
+			acc>>=char_bits;
+			bitacc-=char_bits;
+		}
+	}
+	
+	if(-1!=lower)
+	{
+		acc<<=digit_bits;
+		acc|=hilo[0][lower];
+		bitacc+=7;
+	}
+	
+	while(char_bits<=bitacc)
+	{
+		out.push_back(0xFF & acc);
+		acc>>=char_bits;
+		bitacc-=char_bits;
+	}
+	
 	return;
 }
 }
@@ -59,9 +103,33 @@ void decode(const std::string & in,std::vector<unsigned char> & out)
 
 int main()
 {
-	std::vector<unsigned char> in{0,0,0,0,0,0,0,0,0};
+	for(unsigned n=0, hi=0,lo=0;n<base91::digit_size;++n)
+	{
+		base91::lo[n]='!'+lo;
+		base91::hi[n]='!'+hi;
+		base91::hilo[hi][lo]=n;
+		if(base91::base==(++lo))
+		{
+			hi++;
+			lo=0;
+		}
+	}
+
+	std::vector<unsigned char> in{0,1,2,3,0,0,1,2,0};
 	std::string out;
 	base91::encode(in,out);
+	std::cout<<out<<std::endl;
+	
+	std::vector<unsigned char> test;
+	base91::decode(out,test);
+
+	
+	for(auto n:test)
+		std::cout<<":"<<(short)n;
+	std::cout<<out<<std::endl;
+
+	for(auto n:in)
+		std::cout<<":"<<(short)n;
 	std::cout<<out<<std::endl;
 	
 	return 0;
