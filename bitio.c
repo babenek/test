ef76dcf606081bc9403665e14f5117cb7af25565
babenek <babenek@users.noreply.github.com>
2018-06-14 23:21:22 +0300

    Update bitio.c
    a small fix

--- bitio.c
+++ bitio.c
@@ -8,7 +8,7 @@ for(unsigned n=0;n<=0xF;++n)printf(
 "const unsigned char %c%c%c%c = 0x%X;\n"
 ,n&0x8?'I':'O',n&0x4?'I':'O',n&0x2?'I':'O',n&0x1?'I':'O',n);
 for(unsigned n=0;n<=0xFF;++n)printf(
-"const unsigned char %c%c%c%c%c%c%c%c = 0x%2X;\n"
+"const unsigned char %c%c%c%c%c%c%c%c = 0x%02X;\n"
 ,n&0x80?'I':'O',n&0x40?'I':'O',n&0x20?'I':'O',n&0x10?'I':'O'
 ,n&0x08?'I':'O',n&0x04?'I':'O',n&0x02?'I':'O',n&0x01?'I':'O',n);
 printf("#endif" "\n");
