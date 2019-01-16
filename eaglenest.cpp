b272b5e88e072ba1b579ae4548a3334189e1ca7d
babenek <babenek@gmail.com>
2019-01-16 04:21:42 +0200

    EagleNest

---
+++ eaglenest.cpp
@@ -0,0 +1,43 @@
+//
+// Created by user on 16.01.19.
+//
+
+#include <map>
+#include <iostream>
+
+class Eagle
+{
+	std::map<int, int> entries;
+	int exit;
+
+	int
+	findNest(int egg)
+	{
+		int prev=-1;
+		for (auto n:entries)
+		{
+			if(0>prev)
+			{
+				prev = n->first;
+			}
+			else
+			{
+				if (prev<egg and egg<n->first)
+				{
+					std::cout<<"found:"<<n->first;
+					break;
+				}
+			}
+		};
+	}
+};
+
+int
+main(void)
+{
+	Eagle e;
+	e.entries[2]=0;
+	e.entries[5]=0;
+	e.findNest(3);
+	return 0;
+};
