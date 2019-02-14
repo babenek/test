dfaed77686bc921a9a68bde23e77ab2b46df9fc5
babenek <babenek@gmail.com>
2019-02-14 19:17:18 +0200

    code was format with Allman style

--- eaglenest.cpp
+++ eaglenest.cpp
@@ -5,41 +5,38 @@
 #include <map>
 #include <iostream>
 
-class Eagle
-{
+class Eagle {
 public:
-	std::map<int, int> entries;
-	int exit;
+    std::map<int, int> entries;
+    int exit;
 
-	int
-	findNest(int egg)
-	{
-		int prev=-1;
-		for (auto n:entries)
-		{
-			if(0>prev)
-			{
-				prev = n.first;
-			}
-			else
-			{
-				if (prev<egg and egg<n.first)
-				{
-					std::cout<<"found:"<<n.first;
-					break;
-				}
-			}
-		};
-		return  0;
-	}
+    int
+    findNest(int egg) {
+        int prev = -1;
+        for (auto n:entries)
+        {
+            if (0 > prev)
+            {
+                prev = n.first;
+            }
+            else
+            {
+                if (prev < egg and egg < n.first)
+                {
+                    std::cout << "found:" << n.first;
+                    break;
+                }
+            }
+        };
+        return 0;
+    }
 };
 
 int
-main(void)
-{
-	Eagle e;
-	e.entries[2]=0;
-	e.entries[5]=0;
-	e.findNest(3);
-	return 0;
+main(void) {
+    Eagle e;
+    e.entries[2] = 0;
+    e.entries[5] = 0;
+    e.findNest(3);
+    return 0;
 };
