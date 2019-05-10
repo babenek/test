5497cb544fcd8da4d792a6a54ee904520028f96b
babenek <babenek@gmail.com>
2019-05-10 19:25:25 +0300

    hash 65537

--- hash.cpp
+++ hash.cpp
@@ -21,41 +21,46 @@ unsigned hash(char *string)
 		string++;
 	};
 
-	return result%21111;
+	return 0x1FFFF &( result % 65537);
 }
 
 
-int main_hash()
+int main()
 {
-	unsigned hash_table[21112]={0,};
-	char s[4]={0,};
-	for (char n = 'A'; n <= 'Z'; ++n)
+	unsigned hash_table[65537] = {0,};
+	char s[5] = {0,};
+	for (char k = '0'; k < '3'; ++k)
 	{
-		s[0]=n;
-		for (char m = 'A'; m <= 'Z'; ++m)
+		s[0] = k;
+		for (char n = 'A'; n <= 'Z'; ++n)
 		{
-			s[1] = m;
-			for (char l = 'A'; l <= 'Z'; ++l)
+			s[1] = n;
+			for (char m = 'A'; m <= 'Z'; ++m)
 			{
+				s[2] = m;
+				for (char l = 'A'; l <= 'Z'; ++l)
+				{
 
-				s[2] = l;
-				hash_table[hash(s)]++;
-				//			std::cout << n<<m << ":" << hash(s) << std::endl;
+					s[3] = l;
+					hash_table[hash(s)]++;
+					std::cout << s << ":" << hash(s) << std::endl;
+				}
 			}
 		}
 	}
 
-	unsigned filled=0;
-	for (unsigned n = 0;n<21112;++n)
+
+	unsigned filled = 0;
+	for (unsigned n = 0; n < 65537; ++n)
 	{
-		std::cout << n<<":"<<hash_table[n] << std::endl;
-		if(hash_table[n])
+		std::cout << n << ":" << hash_table[n] << std::endl;
+		if (hash_table[n])
 			filled++;
 	}
-	std::cout << filled/21112.0 << std::endl;
+	std::cout << filled / 65537.0 << std::endl;
 
 
-	return hash(0);
+	return 0;
 
 }
 
