cd93e477d5ef380b1d16b86e207e1bdc946055ca
Roman Babenko <babenek@gmail.com>
2024-07-13 07:35:20 +0300

    as_posix to bytes

--- path_test.py
+++ path_test.py
@@ -3,3 +3,4 @@ from pathlib import Path
 if __name__ == """__main__""":
     p = Path("test") / "1" / "2"
     assert str(p.as_posix()) == "test/1/2", p
+    assert str(p.as_posix()).encode() == b"test/1/2", p
